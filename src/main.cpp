7c8066be6ae1da87621ae2c938ca2764437e1022
Tesla <tesla89karate@gmail.com>
2026-04-29 12:02:23 +0200

    Fix while loop not running

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main()
 
     glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 
-    while (ekran.shouldWindowClose())
+    while (!ekran.shouldWindowClose())
     {
         glClear(GL_COLOR_BUFFER_BIT);
         ekran.swapBuffers();
