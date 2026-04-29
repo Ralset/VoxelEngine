bdfb36399ed0ef166fc44293b0a64e4dc721d749
Tesla <tesla89karate@gmail.com>
2026-04-29 15:42:43 +0200

    Add vertex buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "core/Window.h"
-#include <iostream>
+
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main()
 {
@@ -11,9 +12,21 @@ int main()
 
     glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 
+    float points[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), points, GL_STATIC_DRAW);
+
     while (!ekran.shouldWindowClose())
     {
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         ekran.swapBuffers();
         glfwPollEvents();
     }
