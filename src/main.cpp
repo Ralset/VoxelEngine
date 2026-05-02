026db2e109ac12ecea88dff113e520b4bb0c0cf3
svuskovic22 <tesla89karate@gmail.com>
2026-05-02 23:53:17 +0200

    Progress in learning opengl

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <stdexcept>
 
+#define ASSERT(x) if(!(x)) throw std::runtime_error(std::string("Assert failed: ") + #x + " at line: " + std::to_string(__LINE__) + " at file: " + __FILE__);
 #define GLCall(x)\
     GLClearError();\
     x;\
@@ -78,8 +79,7 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 
 int main()
 {
-    if (!glfwInit())
-        return -1;
+    ASSERT(glfwInit())
 
     Window ekran(640, 480, "Voxel game");
 
@@ -116,16 +116,35 @@ int main()
     std::string vertexShader=ParseShader("assets/shaders/Vertex.shader");
     std::string fragmentShader=ParseShader("assets/shaders/Fragment.shader");
     
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    GLCall(unsigned int shader = CreateShader(vertexShader, fragmentShader));
     GLCall(glUseProgram(shader));
 
-    GLCall(glClearColor(0.1f, 0.2f, 0.3f, 1.0f));
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+    float r = 0.0f;
+    float increment = 0.05f;
 
     while (!ekran.shouldWindowClose())
     {
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
-        
+        r += increment;
+
+        GLCall(glUseProgram(shader));
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        GLCall(glBindVertexArray(vao));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if (r > 1.0f || r < 0.0f)
+            increment *= -1;
+
         ekran.swapBuffers();
         glfwPollEvents();
     }
